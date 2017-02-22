1cbfdee6ffc0df9183052599dfc7883c838d5feb
Muhsin Fatih Yorulmaz <muhsinfatih@gmail.com>
2017-02-22 23:26:04 +0300

    new pwm timing methods

--- quadcopter/pwm.hpp
+++ quadcopter/pwm.hpp
@@ -20,19 +20,49 @@ class pwm {
 private:
 	
 	double _period, _frequency;
+	uint32_t _periodInTicks;
 	uint32_t prescaler;
+	uint32_t clockSpeed = 84000000;
 public:
 	
 	double period() const { return _period; }
 	double frequency() const { return _frequency; }
+	uint32_t periodInTicks() const {return _periodInTicks;}
+	
+	/**
+	 set period in real world terms
+
+	 @param period period in seconds
+	 */
 	void period(const double &period) {
+		_frequency = 1.0 / period;
 		_period = period;
-		_frequency = 0;
+		_periodInTicks = period * clockSpeed / prescaler;
 	}
+	
+	/**
+	 set frequency in real world terms
+
+	 @param frequency frequency (times / sec)
+	 */
 	void frequency(const double &frequency) {
 		_frequency = frequency;
-		_period = 0;
+		_period = 1.0 / frequency;
+		_periodInTicks = clockSpeed / (prescaler * frequency);
 	}
+	
+
+	/**
+	 set period of pwm timer precisely
+	 
+	 @param periodInTicks period in ticks. how many ticks until reset?
+	 */
+	void periodInTicks(const uint32_t &periodInTicks) {
+		_frequency = clockSpeed / (prescaler * periodInTicks);
+		_period = periodInTicks * prescaler / clockSpeed;
+		_periodInTicks = periodInTicks;
+	}
+	
 	
 	GPIO_TypeDef *GPIOx;
 	int numOfPins;
